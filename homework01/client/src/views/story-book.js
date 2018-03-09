66947ef26124b1f96ad4f6ee65100c026d02fd08
arodr856 <arodr856@gmail.com>
2018-03-09 14:27:50 -0800

    story.js isUnlockedYet and unlock functions: working

--- homework01/client/src/views/story-book.js
+++ homework01/client/src/views/story-book.js
@@ -4,6 +4,7 @@ export default function (store) {
 			super();
 			this.store = store;
 			// TODO: initial DOM rendering of story itself
+			this.innerHTML = this.render();
 
 			this.onStateChange = this.handleStateChange.bind(this);
 		}
@@ -18,6 +19,33 @@ export default function (store) {
 
 		disconnectedCallback () {
 			this.store.unsubscribe(this.onStateChange);
+		}
+
+
+		render(){
+			return ` <p>Nam, maecenas. Ligula pulvinar diam, metus venenatis curae; elit egestas interdum pretium nisl etiam. Praesent
+			in mus. Eleifend etiam fusce gravida ac. Sollicitudin iaculis curabitur sapien phasellus. Dictumst eros.
+			Hac ornare augue curabitur. Pede tincidunt. Placerat risus laoreet proin lectus primis commodo In sollicitudin
+			congue interdum cum rutrum felis, gravida congue fringilla. Porta sit ante ullamcorper tempus nam pulvinar
+			tincidunt vivamus euismod fusce pharetra. Ullamcorper augue aptent pretium cum.</p>
+
+			<p>Eu litora nascetur cum duis scelerisque. Potenti habitasse imperdiet ornare congue, orci orci metus dictum.
+			Per tincidunt fermentum orci, lorem urna felis metus pulvinar porta egestas nec viverra nisl elit augue
+			nascetur elementum. Pellentesque dictum mollis elit facilisi. Libero penatibus, venenatis. Justo, tristique
+			porttitor auctor sed leo nulla, class metus penatibus. Molestie scelerisque nec amet gravida. Congue
+			mollis mi ultrices eu. Aptent integer mollis.</p>
+			<p>Nam, maecenas. Ligula pulvinar diam, metus venenatis curae; elit egestas interdum pretium nisl etiam. Praesent
+			in mus. Eleifend etiam fusce gravida ac. Sollicitudin iaculis curabitur sapien phasellus. Dictumst eros.
+			Hac ornare augue curabitur. Pede tincidunt. Placerat risus laoreet proin lectus primis commodo In sollicitudin
+			congue interdum cum rutrum felis, gravida congue fringilla. Porta sit ante ullamcorper tempus nam pulvinar
+			tincidunt vivamus euismod fusce pharetra. Ullamcorper augue aptent pretium cum.</p>
+
+			<p>Eu litora nascetur cum duis scelerisque. Potenti habitasse imperdiet ornare congue, orci orci metus dictum.
+			Per tincidunt fermentum orci, lorem urna felis metus pulvinar porta egestas nec viverra nisl elit augue
+			nascetur elementum. Pellentesque dictum mollis elit facilisi. Libero penatibus, venenatis. Justo, tristique
+			porttitor auctor sed leo nulla, class metus penatibus. Molestie scelerisque nec amet gravida. Congue
+			mollis mi ultrices eu. Aptent integer mollis.</p>`
+			
 		}
 	};
 }
--- homework01/dest/app.bundle.js
+++ homework01/dest/app.bundle.js
@@ -1072,6 +1072,7 @@ exports.default = function (store) {
 			super();
 			this.store = store;
 			// TODO: initial DOM rendering of story itself
+			this.innerHTML = this.render();
 
 			this.onStateChange = this.handleStateChange.bind(this);
 		}
@@ -1086,6 +1087,31 @@ exports.default = function (store) {
 
 		disconnectedCallback() {
 			this.store.unsubscribe(this.onStateChange);
+		}
+
+		render() {
+			return ` <p>Nam, maecenas. Ligula pulvinar diam, metus venenatis curae; elit egestas interdum pretium nisl etiam. Praesent
+			in mus. Eleifend etiam fusce gravida ac. Sollicitudin iaculis curabitur sapien phasellus. Dictumst eros.
+			Hac ornare augue curabitur. Pede tincidunt. Placerat risus laoreet proin lectus primis commodo In sollicitudin
+			congue interdum cum rutrum felis, gravida congue fringilla. Porta sit ante ullamcorper tempus nam pulvinar
+			tincidunt vivamus euismod fusce pharetra. Ullamcorper augue aptent pretium cum.</p>
+
+			<p>Eu litora nascetur cum duis scelerisque. Potenti habitasse imperdiet ornare congue, orci orci metus dictum.
+			Per tincidunt fermentum orci, lorem urna felis metus pulvinar porta egestas nec viverra nisl elit augue
+			nascetur elementum. Pellentesque dictum mollis elit facilisi. Libero penatibus, venenatis. Justo, tristique
+			porttitor auctor sed leo nulla, class metus penatibus. Molestie scelerisque nec amet gravida. Congue
+			mollis mi ultrices eu. Aptent integer mollis.</p>
+			<p>Nam, maecenas. Ligula pulvinar diam, metus venenatis curae; elit egestas interdum pretium nisl etiam. Praesent
+			in mus. Eleifend etiam fusce gravida ac. Sollicitudin iaculis curabitur sapien phasellus. Dictumst eros.
+			Hac ornare augue curabitur. Pede tincidunt. Placerat risus laoreet proin lectus primis commodo In sollicitudin
+			congue interdum cum rutrum felis, gravida congue fringilla. Porta sit ante ullamcorper tempus nam pulvinar
+			tincidunt vivamus euismod fusce pharetra. Ullamcorper augue aptent pretium cum.</p>
+
+			<p>Eu litora nascetur cum duis scelerisque. Potenti habitasse imperdiet ornare congue, orci orci metus dictum.
+			Per tincidunt fermentum orci, lorem urna felis metus pulvinar porta egestas nec viverra nisl elit augue
+			nascetur elementum. Pellentesque dictum mollis elit facilisi. Libero penatibus, venenatis. Justo, tristique
+			porttitor auctor sed leo nulla, class metus penatibus. Molestie scelerisque nec amet gravida. Congue
+			mollis mi ultrices eu. Aptent integer mollis.</p>`;
 		}
 	};
 };
