adb26735ae02cd2a213b4332fcc77768c8b4d668
arodr856 <arodr856@gmail.com>
2018-03-08 22:17:52 -0800

    initial commit

---
+++ homework01/client/src/app.js
@@ -0,0 +1,108 @@
+import '@webcomponents/webcomponentsjs';
+
+import {loop} from './game';
+import Store from './store';
+import reducer from './reducer';
+
+import ButtonComponent from './views/button';
+import CounterComponent from './views/counter';
+import ExampleComponent from './views/example';
+import GeneratorComponent from './views/generator';
+import StoryBookComponent from './views/story-book';
+
+import Generator from "./models/generator";
+
+/**
+ * Data flow diagram
+ +----------------------------------------------------+
+ | +------------------+          +------------------+ |
+ | |                  |          |                  | |
+++-|       Loop       |<---------|    Generator     | |
+|| |                  |          |                  | |
+|| +------------------+          +------------------+ |
+||G          ^                                        |
+||a          +-----------------------------+          |
+||m                                        |          |
+||e                                        |          |
+||                               +------------------+ |
+||                               |                  | |
+||                               |     Stories      | |
+||                               |                  | |
+||                               +------------------+ |
+|+----------------------------------------------------+
++------------------------------------------------------------+
+|                                                            |
+|                                                            |
+|                                                            |
+|                                                            |
+|                                                            |
+|                                                            |
+|                                                            |
+|                                                            |
+|       +----------------------------------------------------+----------+
+|       | +------------------+                     +------------------+ |
+|       | |                  |        Mutates      |                  | |
+|       | |     Reducer      |-------------------->|      State       | |
+|       | |                  |                     |                  | |
+|       | +------------------+                     +------------------+ |
+|       |S          ^                                        |          |
+|       |t          |                                        |          |
+|       |o          |                                        |          |
+|       |r          | Triggers                     Notifies  |          |
+|       |e          |                                        |          |
+|       |           |                                        v          |
+|       | +------------------+                     +------------------+ |
+|       | |                  |                     |                  | |
++-------+>|      Action      |                     |    Listeners     | |
+        | |                  |                     |                  | |
+        | +------------------+                     +------------------+ |
+        +-----------^----------------------------------------+----------+
+                    |                                        |
+                    |                                        |
+                    |                                        |
+                    |                                        |
+                    | Dispatches                             |
+                    |                                        |
+                    |                                        |
+          +------------------+                               |
+          |                  |                               |
+          |      Views       |              Notifies changes |
+          |    Components    |<------------------------------+
+          |                  |
+          +------------------+
+          */
+          main();
+
+// main function wraps everything at top level
+function main () {
+	const initialState = {
+		example: 'Hello custom element',
+		counter: 0,
+		generators: [{type: 'autonomous',name: "tree",description: 'desc',rate: 10,quantity: 0, baseCost: 10,  unlockValue: 10},
+		{type: 'autonomous',name: "factory",description: 'desc',rate: 25,quantity: 0, baseCost: 35,  unlockValue: 35},
+		{type: 'autonomous',name: "rain",description: 'desc',rate: 45,quantity: 0, baseCost: 70,  unlockValue: 70}
+		],
+		story: []
+	};
+
+	// initialize store
+	const store = new Store(reducer, initialState);
+	console.log(ExampleComponent(store));
+
+	// define web components
+	window.customElements.define('component-example', ExampleComponent(store));
+	// no longer used
+	window.customElements.define('game-button', ButtonComponent(store));
+	window.customElements.define('game-counter', CounterComponent(store));
+	// lab 3
+	window.customElements.define('game-generator', GeneratorComponent(store));
+	// homework 1
+	window.customElements.define('game-story-book', StoryBookComponent(store));
+
+	// For ease of debugging purpose, we will expose the critical store under window
+	// ps: window is global
+	window.store = store;
+
+	// start game loop
+	loop(store);
+}
---
+++ homework01/client/src/views/button.js
@@ -0,0 +1,20 @@
+export default function (store) {
+	return class ButtonComponent extends window.HTMLElement {
+		constructor () {
+			super();
+			this.store = store;
+		}
+
+		connectedCallback () {
+			this.addEventListener('click', () => {
+				this.store.dispatch({type: 'BUTTON_CLICK'});
+			});
+		}
+
+		disconnectedCallback () {
+			this.store.unsubscribe(this.onStateChange);
+		}
+
+
+	};
+}
