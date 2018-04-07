6c4f1d682e0498f31a9e03a7a4218133d91fddd5
arodr856 <arodr856@gmail.com>
2018-04-07 14:37:09 -0700

    change to EventsDAOEImplTest

--- lab4/src/test/java/edu/csula/storage/servlet/EventsDAOImplTest.java
+++ lab4/src/test/java/edu/csula/storage/servlet/EventsDAOImplTest.java
@@ -1,30 +1,36 @@
 package edu.csula.storage.servlet;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.ServletContext;
 
-import org.junit.Before;
-import org.junit.Test;
-
 import edu.csula.models.Event;
 import edu.csula.storage.EventsDAO;
+
+import org.junit.*;
+
+import org.mockito.stubbing.Answer;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class EventsDAOImplTest {
 	private ServletContext context;
 	private EventsDAO dao;
+	private List<Event> list;
 
 	@Before
 	public void setup() {
 		context = mock(ServletContext.class);
+		doAnswer((Answer) invocation -> {
+			return list;
+		}).when(context).getAttribute(eq(EventsDAOImpl.CONTEXT_NAME));
+		doAnswer((Answer) invocation -> {
+			list = (List<Event>) invocation.getArgument(1);
+			return null;
+		}).when(context).setAttribute(eq(EventsDAOImpl.CONTEXT_NAME), any(List.class));
 		dao = new EventsDAOImpl(context);
 	}
 
@@ -66,31 +72,26 @@ public class EventsDAOImplTest {
 		// assert and verify
 		verify(context).getAttribute(EventsDAOImpl.CONTEXT_NAME);
 		assertTrue(actualEvent.isPresent());
-		assertEquals(new Event(1, "name", "desc", 10), actualEvent.get());
+		assertEquals(actualEvent.get(), new Event(1, "name", "desc", 10));
 	}
 
 	@Test
 	public void set() throws Exception {
 		// set up initial mock state
-		Collection<Event> mockEvents = new ArrayList<>();
-		mockEvents.add(new Event(1, "name", "desc", 10));
-		when(context.getAttribute(EventsDAOImpl.CONTEXT_NAME)).thenReturn(mockEvents);
+		dao.add(new Event(1, "name", "desc", 10));
 
 		// actual method execution
 		dao.set(1, new Event(1, "new name", "description", 20));
 		Optional<Event> actualEvent = dao.getById(1);
 
 		// assert and verify
-		verify(context).getAttribute(EventsDAOImpl.CONTEXT_NAME);
 		assertTrue(actualEvent.isPresent());
-		assertEquals(actualEvent.get(), new Event(1, "new name", "description", 20));
+		assertEquals(new Event(1, "new name", "description", 20), actualEvent.get());
 	}
 
 	@Test
 	public void add() throws Exception {
 		// set up
-
-		when(context.getAttribute(EventsDAOImpl.CONTEXT_NAME)).thenReturn(null);
 		Collection<Event> expected = new ArrayList<>();
 		expected.add(new Event(1, "new event", "description", 10));
 		// actual execution
