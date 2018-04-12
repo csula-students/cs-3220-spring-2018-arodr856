d6d9bdd6bce23620ad4d7b6e70b37c86c47e19e5
arodr856 <arodr856@gmail.com>
2018-04-12 14:33:15 -0700

    added homework02 folder

---
+++ homework02/src/main/java/edu/csula/web/AdminGeneratorsServlet.java
@@ -0,0 +1,32 @@
+package edu.csula.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/generators")
+public class AdminGeneratorsServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		// TODO: render the generators page HTML
+		out.println("<h1>Welcome to the Generators page!</h1>");
+		out.println("<a href='events'>Link To Events Page</a>");
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO: handle upsert transaction
+	}
+}
---
+++ homework02/src/main/java/edu/csula/web/DeleteServlet.java
@@ -0,0 +1,61 @@
+package edu.csula.web;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.csula.models.Event;
+import edu.csula.storage.servlet.EventsDAOImpl;
+
+/**
+ * Servlet implementation class DeleteServlet
+ */
+@WebServlet("/admin/DeleteServlet")
+public class DeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DeleteServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		EventsDAOImpl dao = (EventsDAOImpl) getServletContext().getAttribute("dao");
+		Collection<Event> temp = dao.getAll();
+		Iterator<Event> iterator = temp.iterator();
+		while (iterator.hasNext()) {
+			Event event = iterator.next();
+			if (event.getId() == id) {
+				iterator.remove();
+				break;
+			}
+		}
+		response.sendRedirect("events");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
